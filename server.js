9ebe27a84f7df1005c817ac6289adb1a60a01b2c
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-03-04 14:13:03 -0600

    Updated view engine and views location

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const { connectDB } = require('./src/db')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./src/graphql/schema')
+const path = require('path')
 
 dotenv.config()
 
@@ -14,6 +15,12 @@ app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// update location of views folder that res.render pulls from
+app.set('views', path.join(__dirname, '/src/templates/views'));
 
 /* Initialize Routes */
 require("./src/routes")(app)
